761b2cf1cc085e86618f50ae5a9c823ca450ceae
Camille Brasselet <xcamille2605x@gmail.com>
2024-06-10 18:28:06 +0200

    Mise à jour du model Task pour ajouter la date limite pour chaque tâche

--- app/src/main/java/com/openclassrooms/myrepo/data/service/TaskApi.java
+++ app/src/main/java/com/openclassrooms/myrepo/data/service/TaskApi.java
@@ -3,10 +3,12 @@ package com.openclassrooms.myrepo.data.service;
 import com.openclassrooms.myrepo.model.Task;
 
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
 
 /**
- * Cette classe simule la récupération de tâches depuis une API.
+ * Cette classe simule la récupération de tâches depuis une API (Interface de programmation d'application)
  * Les tâches générées sont utilisées à des fins de démonstration.
  */
 public class TaskApi {
@@ -18,16 +20,39 @@ public class TaskApi {
     public List<Task> getTasks() {
         // Simule la récupération des tâches depuis une API
         List<Task> tasks = new ArrayList<>();
-        tasks.add(new Task("Faire les courses pour le dîner"));
-        tasks.add(new Task("Préparer le rapport pour la réunion"));
-        tasks.add(new Task("Répondre aux e-mails en attente"));
-        tasks.add(new Task("Faire de l'exercice pendant 30 minutes"));
-        tasks.add(new Task("Planifier les vacances d'été"));
-        tasks.add(new Task("Rendre le livre à la bibliothèque"));
-        tasks.add(new Task("Réviser pour l'examen de mathématiques"));
-        tasks.add(new Task("Appeler le plombier pour la fuite d'eau"));
-        tasks.add(new Task("Nettoyer le garage"));
-        tasks.add(new Task("Préparer une liste de courses"));
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.setTime(new Date());// Date actuelle
+
+        tasks.add(new Task("Faire les courses pour le dîner",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Préparer le rapport pour la réunion",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Répondre aux e-mails en attente",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Faire de l'exercice pendant 30 minutes",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Planifier les vacances d'été",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Rendre le livre à la bibliothèque",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Réviser pour l'examen de mathématiques",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Appeler le plombier pour la fuite d'eau",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Nettoyer le garage",calendar.getTime()));
+
+        calendar.add(Calendar.DAY_OF_YEAR,1);// Ajoute 1 jour
+        tasks.add(new Task("Préparer une liste de courses",calendar.getTime()));
+
         return tasks;
     }
 }
--- app/src/main/java/com/openclassrooms/myrepo/model/Task.java
+++ app/src/main/java/com/openclassrooms/myrepo/model/Task.java
@@ -1,20 +1,47 @@
 package com.openclassrooms.myrepo.model;
 
+import java.util.Date;
 import java.util.Objects;
 
 /**
- * Une classe représentant une tâche avec une description.
+ * Classe représentant une tâche avec sa description et sa date d'échéance.
  */
 public class Task {
     private String description;
 
+    private Date dueTime;
+
     /**
-     * Constructeur pour créer une nouvelle tâche avec une description.
-     *
-     * @param description La description de la tâche.
+     * Obtient la date d'échéance de la tâche.
+
+     * @return La date d'échéance de la tâche.
      */
-    public Task(String description) {
+    public Date getDueTime() {
+
+        return dueTime;
+    }
+
+    /**
+     * Définit la date d'échéance de la tâche.
+     *
+     * @param dueTime La nouvelle date d'échéance de la tâche.
+     */
+    public void setDueTime(Date dueTime) {
+
+        this.dueTime = dueTime;
+    }
+
+    /**
+     * Constructeur pour créer une nouvelle tâche sa description et sa date d'échéance.
+     *     * @param description La description de la tâche.
+     *     * @param dueTime La date d'échéance de la tâche.
+     */
+    public Task(String description, Date dueTime) {
+
         this.description = description;
+
+        this.dueTime = dueTime;
+
     }
 
     /**
@@ -36,26 +63,25 @@ public class Task {
     }
 
     /**
-     * Vérifie si deux objets Task sont égaux en comparant leurs descriptions.
-     *
-     * @param o L'objet à comparer.
-     * @return Vrai si les descriptions sont égales, sinon faux.
-     */
+     * Vérifie si deux objets Task sont égaux en comparant leurs descriptions et leurs dates d'échéance.
+     *     * @param o L'objet à comparer.
+     *     * @return Vrai si les descriptions et les dates d'échéance sont égales, sinon faux.
+    */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Task task = (Task) o;
-        return Objects.equals(description, task.description);
+        return Objects.equals(description, task.description) && Objects.equals(dueTime,task.dueTime);
     }
 
     /**
-     * Calcule le code de hachage en utilisant la description de la tâche.
-     *
-     * @return Le code de hachage calculé.
+     * Calcule le code de hachage en utilisant la description et la date d'échéance de la tâche.
+     *     * @return Le code de hachage calculé.
      */
     @Override
     public int hashCode() {
-        return Objects.hash(description);
+
+        return Objects.hash(description, dueTime);
     }
 }
