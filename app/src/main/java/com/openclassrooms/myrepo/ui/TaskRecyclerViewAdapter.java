be844ade19fc7b9040c3096d7d0d4a32abc2a72e
Camille Brasselet <xcamille2605x@gmail.com>
2024-06-10 22:37:32 +0200

    Mise à jour du layout 'task_item' et du 'TaskRecyclerViewAdapter' pour afficher la date limite à l'écran ainsi qu'une barre de progression.

--- app/src/main/java/com/openclassrooms/myrepo/ui/TaskRecyclerViewAdapter.java
+++ app/src/main/java/com/openclassrooms/myrepo/ui/TaskRecyclerViewAdapter.java
@@ -8,8 +8,14 @@ import androidx.recyclerview.widget.DiffUtil;
 import androidx.recyclerview.widget.ListAdapter;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.android.material.progressindicator.LinearProgressIndicator;
 import com.openclassrooms.myrepo.R;
 import com.openclassrooms.myrepo.model.Task;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 /**
  * Un adaptateur pour afficher la liste de tâches dans un RecyclerView.
@@ -38,9 +44,13 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
     /**
      * ViewHolder pour afficher les éléments de la liste de tâches.
      */
-    static class ViewHolder extends RecyclerView.ViewHolder {
+    public static class ViewHolder extends RecyclerView.ViewHolder {
 
         private final TextView factTextView;
+
+        private final TextView dueTimeTextView;
+
+        private final LinearProgressIndicator progressIndicator;
 
         /**
          * Constructeur du ViewHolder.
@@ -48,6 +58,8 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
         public ViewHolder(@NonNull View itemView) {
             super(itemView);
             factTextView = itemView.findViewById(R.id.task_description);
+            dueTimeTextView = itemView.findViewById(R.id.task_duetime);
+            progressIndicator = itemView.findViewById(R.id.progress_horizontal);
         }
 
         /**
@@ -57,6 +69,39 @@ public class TaskRecyclerViewAdapter extends ListAdapter<Task, TaskRecyclerViewA
          */
         public void bind(Task task) {
             factTextView.setText(task.getDescription());
+            //instancie la classe SimpleDateFormat pour afficher les dates sous formes "jours/mois/année"
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy", Locale.getDefault());
+            //je formate la date d'échéance en une chaine de caractère
+            String formattedDueTime = dateFormat.format(task.getDueTime());
+            //met a jour dueTimeTextView et affiche une chaîne descriptive suivie de la date d'échéance.
+            dueTimeTextView.setText("Date limite :" + formattedDueTime);
+            //calcul la progression d'une tâche en fonction de sa date d'échéance et met à jour l'indicateur
+            int progress = calculateProgress(task.getDueTime());
+            progressIndicator.setProgress(progress);
+        }
+
+        /**
+         * Calcule la valeur de progression en pourcentage pour la barre de progression.
+         * @param dueTime La date d'échéance de la tâche.
+         * @return La valeur de progression en pourcentage.
+         */
+        private int calculateProgress(Date dueTime){
+            Calendar calendarToday = Calendar.getInstance();//initialise la date du jour sans les heures, minutes,...
+            calendarToday.set(Calendar.HOUR_OF_DAY,0);
+            calendarToday.set(Calendar.MINUTE,0);
+            calendarToday.set(Calendar.SECOND,0);
+            calendarToday.set(Calendar.MILLISECOND,0);
+
+            Calendar calendarDueTime = Calendar.getInstance();////initialise la date d'échéance sans les heures, minutes,...
+            calendarDueTime.setTime(dueTime);
+            calendarDueTime.set(Calendar.HOUR_OF_DAY,0);
+            calendarDueTime.set(Calendar.MINUTE,0);
+            calendarDueTime.set(Calendar.SECOND,0);
+            calendarDueTime.set(Calendar.MILLISECOND,0);
+
+            int daysDifference = (int) //calcule entre les 2 dates en miliseconde pour retourner le résultat en jours
+                    ((calendarDueTime.getTimeInMillis()- calendarToday.getTimeInMillis()) / (24*60*60*1000));
+            return 100 - (daysDifference * 10); //calcule en pourcentage
         }
     }
 
